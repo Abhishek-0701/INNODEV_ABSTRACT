3eeba7721b17c96d50992ab4e27704ea3c92ed31
Abhishek kumar <abhishekkumar30350@gmail.com>
2024-03-14 14:06:22 +0530

    My third commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BrowserRouter as Router } from "react-router-dom"
 import AppRoutes from "./AppRoutes";
+import Auth0providerWithNavigate from './auth/Auth0providerWithNavigate';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <Auth0providerWithNavigate>
       <AppRoutes/>
+      </Auth0providerWithNavigate>
+      
     </Router>
   </React.StrictMode>
 );
