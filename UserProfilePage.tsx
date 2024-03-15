fd837b9fd4f1d31f674e222406f7088347fa802d
Abhishek-0701 <129402170+Abhishek-0701@users.noreply.github.com>
2024-03-15 20:04:55 +0530

    Delete UserProfilePage.tsx

--- UserProfilePage.tsx
+++
@@ -1,8 +0,0 @@
-import UserProfileForm from "@/forms/user-profile-form/UserProfileForm"
-
-const UserProfilePage = () =>{
-      return <UserProfileForm/>;
-      
-};
-
-export default UserProfilePage;
