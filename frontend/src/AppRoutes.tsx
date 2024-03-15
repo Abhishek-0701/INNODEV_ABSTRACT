847f13d8a9b27fa78d6acf8d8c2430e154fd6e90
Abhishek kumar <abhishekkumar30350@gmail.com>
2024-03-15 19:58:42 +0530

    Create backend api for user profile page and frontend page of form

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -2,13 +2,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import Homepage from "./pages/Homepage";
 import AuthCallbackPage from "./pages/AuthCallbackpage";
+import UserProfilePage from "./pages/UserProfile";
 
 const AppRoutes =() =>{
     return(
         <Routes>
-            <Route path="/" element={<Layout><Homepage/></Layout>}/>
+            <Route path="/" element={<Layout showHero><Homepage/></Layout>}/>
             <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
-            <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>}/>
+            <Route path="/user-profile" element={<Layout><UserProfilePage/></Layout>}/>
             <Route path="*" element={<Navigate to="/" />}/>
         </Routes>
     );
--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,13 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
-
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetMyUser = () => {
+};
 
 type CreateUserRequest = {
   auth0Id: string;
   email: string;
 };
-
 
 export const useCreateMyUser=()=>{
   const { getAccessTokenSilently } = useAuth0();
@@ -44,4 +45,54 @@ export const useCreateMyUser=()=>{
     isSuccess,
   };
 };
+
+type useUpdateMyUser={
+
+  name:string;
+  addressLine1:string;
+  city:string;
+  country:string;
+
+
+}
+
+
+
+
+
+export const useUpdateMyUser=()=>{
+
+  const {getAccessTokenSilently}=useAuth0();
+  
+ const updateMyUserRequest=async (formData:useUpdateMyUser)=>{
+
+    const accessToken=await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`,{
+      method:"PUT",
+      headers:{
+        Authorization:`Bearer ${accessToken}`,
+        "Content-Type":"application/json",
+      },
+      body:JSON.stringify(formData),
+
+    });
+    if(!response.ok)
+    {
+      throw new Error ("Failed to update ");
+    }
+
+
+  };
+
+  const {mutateAsync:updateUser,isLoading,isSuccess,isError,reset}=useMutation(updateMyUserRequest)
+
+
+  return {updateUser,isLoading};
+
+};
+
+
+
+
+
 
